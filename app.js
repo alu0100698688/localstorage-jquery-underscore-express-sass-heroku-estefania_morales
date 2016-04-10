4557d5920bc5e00bf3b8759c84b846d9efbe0412
alu0100698688 <alu0100698688@ull.edu.es>
2016-04-10 20:33:53 +0100

    Creación de la estructura views. Modificada app.js conforme a lo solicitado. No funciona tras post.

--- app.js
+++ app.js
@@ -2,17 +2,37 @@ var _ = require('underscore');
 var $ = require('jquery');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+var path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 require('ejs').delimiter = '$';
 
+var expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layout'); // defaults to 'layout'  '
+
+// Serve static files
+app.use(express.static('.')); // http://expressjs.com/api.html#app.use#
+app.use(expressLayouts);
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {title: 'CSV separated values'});
 
 });
 
-app.listen(5000, function(){
-  console.log('Example app listening');
+app.post('/csv', function(req, res){
+  var original = req.body.original;
+  res.render('resultado', {original: original, title: 'Resultado'});
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
 
 module.exports = app;
