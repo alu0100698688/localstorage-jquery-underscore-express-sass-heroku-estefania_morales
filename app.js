365015c34ca65dc52a829483d012a90da7ea8e98
alu0100698688 <alu0100698688@ull.edu.es>
2016-04-10 02:53:27 +0100

    Creada carpeta views con el index y modificada app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+var _ = require('underscore');
+var $ = require('jquery');
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index');
 
 });
 
 app.listen(5000, function(){
   console.log('Example app listening');
 });
+
+module.exports = app;
